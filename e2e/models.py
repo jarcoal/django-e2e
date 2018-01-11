72795bfc0de71cb92aee2fb94a93d7111775eeca
Jared Morse <jarcoal@gmail.com>
2018-01-11 15:26:57 -0800

    initial User model

--- e2e/models.py
+++ e2e/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser as DjangoAbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class PublicKeyCryptoMixin(models.Model):
+    """Model mixin that adds fields to store an encrypted private key and
+       it's corresponding public key."""
+
+    enc_private_key = models.TextField(
+        _('Encrypted Private Key'),
+        help_text=_('Base64 encoded private key encrypted by a password '
+                    'derived symmetric key.'))
+
+    public_key = models.TextField(
+        _('Public Key'),
+        help_text=_('Base64 encoded public key'))
+
+    class Meta:
+        abstract = True
+
+
+class User(PublicKeyCryptoMixin, DjangoAbstractUser):
+    """User model that supports storing the user's encrypted private key and
+       it's corresponding public key"""
--- test_project/settings.py
+++ test_project/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+AUTH_USER_MODEL = 'e2e.User'
+
 # Application definition
 
 INSTALLED_APPS = [
